6ae1770990b4b574e62edb417cc55505c6c38862
Anna Lukinykh <lychok@list.ru>
2016-01-14 18:29:12 +0600

    add callback function

--- main.js
+++ main.js
@@ -18,15 +18,25 @@ Person.prototype.getCycle = function() {
 };
 
 Person.prototype.getPassword = function() {
-    return this._getHashPassword();
+    return this._getHashPassword(getHash);
 };
 
-Person.prototype._getHashPassword = function() {
+function getHash(hash) {
+    console.log(hash);
+}
+
+Person.prototype._getHashPassword = function(callback) {
     var hash = this.password;
-    var cycle = this.getCycle();
+    var cycle = this.cycle;
+    var width = 0;
     while(cycle) {
         hash = md5(hash);
-        cycle--;
+        cycle -=1;
+        callback(hash);
+        while(width < 100) {
+            width += 100/cycle;
+            $('.progress-bar').css('width', width + '%');
+        }
     }
     return hash;
 };
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 QUnit.test("test", function() {
-    var person = new Person('anna', 'aaa@gmail.com', '123456', '3');
+    var person = new Person('anna', 'aaa@gmail.com', '123456', 3);
     var hash = person.getPassword();
     equal(person.getUsername(), 'anna', 'Passed!');
     equal(person.getEmail(), 'aaa@gmail.com', 'Passed!');
-    equal(person.getCycle(), '3', 'Passed!');
+    equal(person.getCycle(), 3, 'Passed!');
     equal(person.getPassword(), hash, 'Passed!');
 });
