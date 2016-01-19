9cc87f216141d4fcaa6c8450866ecfeb040f2f7d
Anna Lukinykh <lychok@list.ru>
2016-01-19 18:48:43 +0600

    add  test

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-var person = new Person('anna', 'aaa@gmail.com', '123456', 3),
-    hash = person.getPassword();
+var person = new Person('anna', 'aaa@gmail.com', '123456', 3);
+    person.getPassword();
 QUnit.test("test", function() {
     equal(person.getUsername(), 'anna', 'Passed!');
     equal(person.getEmail(), 'aaa@gmail.com', 'Passed!');
     equal(person.getCycle(), 3, 'Passed!');
+    notEqual(person.getUsername(), 135, 'Passed!');
+    notEqual(person.getEmail(), 'sdas@vfddf', 'Passed!');
+    notEqual(person.getCycle(), 'anna', 'Passed!');
 });
 
-QUnit.asyncTest( "asynchronous test", function() {
+QUnit.asyncTest('asynchronous test', function() {
+    console.log(person.getPassword());
     setTimeout(function() {
-        ok(hash, "Passed and ready to resume!" );
+        equal(person.getPassword().done(function() { return 'success';}), 'success', "Passed and ready to resume!");
         start();
     }, 1000);
+});
+
+QUnit.done(function( details ) {
+    console.log( "Total: ", details.total, " Failed: ", details.failed, " Passed: ", details.passed, " Runtime: ", details.runtime );
 });
