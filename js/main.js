1b76cb4298c1c2e674ea927cf1353bab74c197fc
Anna Lukinykh <lychok@list.ru>
2016-01-22 11:49:19 +0600

    fix bugs

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+//        $('input').val('');
+    $('.hash').on('click', function() {
+        $('.has-error').remove();
+        $('.progress-bar').css('width', '0%');
+        var person = new Person($('#username').val(), $('#email').val(), $('#pwd').val(), $('#cycle').val());
+        person.getPassword();
+    })
+});
